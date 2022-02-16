83c7c61137104bf2618cc31e64db476bd2378c97
Av3nirr <caillettheo1@gmail.com>
2022-02-16 19:22:27 +0100

    Adding utility button

--- buttons/utiliti.js
+++ buttons/utiliti.js
@@ -1,5 +1,5 @@
 const { Interaction, MessageEmbed, Client } = require('discord.js');
-
+const { ticketChannel } = require('../config.json')
 
 module.exports = {
     /**
@@ -8,7 +8,19 @@ module.exports = {
      * @param {Interaction} interaction 
      */
     async execute(client, interaction) {
-        await interaction.reply({content: "salut"})
+        var ticket = interaction.guild.channels.cache.get(ticketChannel)
+        const embed = new MessageEmbed()
+            .setTitle("Message d'aide: Commandes Utiles")
+            .setColor("PURPLE")
+            .setDescription("Voici la liste de toutes le commandes utiles du bot")
+            .setFooter({
+                text: "Aurora Bot"
+            })
+            .addField("__/help__", "Affiche le message d'aide principal !")
+            .addField('__/ping__', "Affiche le temps de réponse du bot !")
+            .addField('<#' + ticketl + '>', "Créer un ticket pour contacter le staff !")
+            .setTimestamp()
+        await interaction.reply({embeds: [embed]})
     }
 
 }
